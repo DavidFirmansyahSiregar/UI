03ce835eca4ae2983ee18631c5efb3f9a26a029d
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2022-12-23 13:02:21 +0700

    App

--- src/App.js
+++ src/App.js
@@ -2,3 +2,16 @@ import React from "react";
 import { ChatEngine } from "react-chat-engine";
 
 import ".App.css";
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            ProjectID=""
+            userName=""
+            userSecret=""
+        />
+    );
+}
+
+export default App;
