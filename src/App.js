8caf1b14f391014844ed14a4a8545ad62bf22f74
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2023-01-11 14:51:03 +0700

    jadi

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { ChatEngine } from "react-chat-engine";
 
-import ".App.css";
+import "./App.css";
 
 const App = () => {
     return (
         <ChatEngine 
             height="100vh"
-            ProjectID=""
-            userName=""
-            userSecret=""
+            projectID="0772835b-f0a9-4223-94d0-6d17331d5c69"
+            userName="bambang"
+            userSecret="abcde"  
         />
     );
 }
